bf50dc767107a45db4b18572f0350ad4b80fb953
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-01-12 12:37:01 -0800

    build will now run

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const TodoItem = ({todo, idx, updateTodo, deleteTodo}) => {
 
-    const [style, setStyle] = useState({});
+
 
     const checked = {
         'text-decoration': 'line-through'
