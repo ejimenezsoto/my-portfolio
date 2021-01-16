da1be9fe438ffdd5338352948aace375517112f1
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-01-15 20:28:11 -0800

    Updated Portfolio

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
 import Project from "./components/Project";
 import NavBar from "./components/NavBar";
-import { Helmet } from 'react-helmet'
-import Todo from "./components/Todo"
 
 
 const TITLE = "Enzo's Portfolio Website"
@@ -15,17 +13,14 @@ function App() {
   return (
     
     <BrowserRouter>
-    <Helmet>
-          <title>{ TITLE }</title>
-    </Helmet>
     <NavBar />
+
     <Switch>
       <Route component={Home} path='/' exact />
       <Route component={About} path='/about' />
       <Route component={SinglePost} path='/post/:slug' />
       <Route component={Post} path='/post' />
       <Route component={Project} path='/project' />
-      <Route component={Todo} path='/todolist' />
     </Switch>
     </BrowserRouter>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,38 +5,33 @@ import { SocialIcon } from "react-social-icons"
 export default function NavBar() {
     return (
         <header className="header-color">
-            <div className="container mx-auto flex justify-between">
-                <nav className="flex">
+            <div className="container mx-auto flex justify-between ml-0 mr-0">
+                <nav className="flex ml-0 mr-0">
                     <NavLink to='/' 
                     exact
                     activeClassName="text-white"
-                    className="inline-flex items-center py-6 px-3 mr-4 hover:text-green-800 text-6xl font-bold cursive tracking-widest">
-                        Enzo
+                    className="inline-flex items-center  px-3 text-red-100 hover:text-green-800 text-6xl font-bold cursive tracking-widest">
+                        ENZO
                     </NavLink>
                     <NavLink to="/post" 
                     activeClassName="text-white"
-                    className="inline-flex items-center py-3 px-3 my-6 text-xl text-white-200 hover:text-green-800">
+                    className="inline-flex items-center py-3 px-3  rounded text-black hover:text-green-800">
                         Blog Post
                     </NavLink>
                     <NavLink to='/project' 
                     activeClassName="text-white"
-                    className="inline-flex items-center py-3 px-3 my-6 text-xl text-white-200 hover:text-green-800">
+                    className="inline-flex items-center py-3 px-3  rounded text-black hover:text-green-800">
                         Projects
                     </NavLink>
                     <NavLink to='/about'
                     activeClassName="text-white"
-                    className="inline-flex items-center py-3 px-3 my-6 text-xl text-white-200 hover:text-green-800">
+                    className="inline-flex items-center py-3 px-3  rounded text-black hover:text-green-800">
                         About Me
                     </NavLink>
-                    <NavLink to='/todolist'
-                    activeClassName="text-white"
-                    className="inline-flex items-center py-3 px-3 my-6 text-xl text-white-200 hover:text-green-800">
-                        Todo-List
-                    </NavLink>
                 </nav>
-                <div className="inline-flex py-3 px-3 my-6">  
-                    <SocialIcon url="https://github.com/ejimenezsoto" className='mr-4' target="_blank" fgColor="#fff" style={{height:30, width:30}} />
-                    <SocialIcon url="https://www.linkedin.com/in/enzojimenezsoto/" className='mr-4' target="_blank" fgColor="#fff" style={{height:30, width:30}} />
+                <div className="inline-flex items-center py-3 px-3">  
+                    <SocialIcon url="https://github.com/ejimenezsoto" className='mr-4' target="_blank" fgColor="#fff" style={{height:35, width:35}} />
+                    <SocialIcon url="https://www.linkedin.com/in/enzojimenezsoto/" className='mr-4' target="_blank" fgColor="#fff" style={{height:35, width:35}} />
                 </div>
             </div>
         </header>
